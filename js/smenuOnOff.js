0c2ca2c34907437fae379e97bb65f883eb94ebc3
monggu1004 <7243527@naver.com>
2024-08-08 18:21:17 +0900

    secondpage
    second

---
+++ js/smenuOnOff.js
@@ -0,0 +1,7 @@
+// const smenuBars = document.querySelector(".fa-bars");
+// const navMenu = document.querySelector("nav");
+// function smenuOnOff() {
+//   navMenu.classList.toggle("hidden");
+// }
+
+// smenuBars.addEventListener("click", smenuOnOff);
