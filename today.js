c5773417696ab756c2dae7b2770536a71f6a063f
monggu1004 <7243527@naver.com>
2024-08-30 14:00:40 +0900

    fin
    fin

--- today.js
+++ today.js
@@ -1,31 +1,28 @@
-console.log("sdf");
+// let todaywordJA = document.querySelector(".todaywordJA");
+// let todaywordKO = document.querySelector(".todaywordKO");
+// let todaywordAN = document.querySelector(".todaywordAN");
 
-let todaywordJA = document.querySelector(".todaywordJA");
-let todaywordKO = document.querySelector(".todaywordKO");
+// let todaywords = [
+//   {
+//     japanword: "일본어 1",
+//     koreaword: "한국어 1",
+//   },
+//   {
+//     japanword: "일본어 2",
+//     koreaword: "한국어 2",
+//   },
+//   {
+//     japanword: "일본어 3",
+//     koreaword: "한국어 3",
+//   },
+// ];
+// let todaydate = new Date();
+// let todaylength;
+// let randomindex;
 
-let todaywords = [
-  {
-    japanword: "일본어 1",
-    koreaword: "한국어 1",
-  },
-  {
-    japanword: "일본어 2",
-    koreaword: "한국어 2",
-  },
-  {
-    japanword: "일본어 3",
-    koreaword: "한국어 3",
-  },
-];
-console.log("todaywords");
+// todaylength = todaywords.length;
+// randomindex = Math.floor(Math.random() * todaylength);
+// todaywordJA.innerText = todaywords[randomindex].japanword;
+// todaywordKO.innerText = todaywords[randomindex].koreaword;
 
-function startword() {
-  let todaylength = todaywords.length;
-  let randomindex = Math.floor(Math.random() * todaylength);
-
-  todaywordJA.innerText = todaywords[randomindex].japanword;
-  todaywordKO.innerText = todaywords[randomindex].koreaword;
-}
-startword();
-
-document.addEventListener("DOMContentLoaded", startword);
+// todaywordAN.addEventListener("click", showword);
--- usage/usecover.js
+++ usage/usecover.js
@@ -1,16 +1,1 @@
-const clear = document.querySelector(".clear");
-const fail = document.querySelector(".fail");
 
-function clearcover() {
-  clear.classList.add("coverbutton");
-  fail.classList.remove("coverbutton");
-}
-function failcover() {
-  clear.classList.remove("coverbutton");
-  fail.classList.add("coverbutton");
-  showfailbox();
-}
-let saveds = JSON.parse(localStorage.getItem("saveword"));
-
-clear.addEventListener("click", clearcover);
-fail.addEventListener("click", failcover);
